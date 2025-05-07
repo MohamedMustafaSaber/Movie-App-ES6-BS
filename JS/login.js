e2a3cacd8ff10e73983e20d21882251f460332c2
Mohamed Mostafa Saber <MohamedMustafaSaber>
2025-05-08 01:24:51 +0300

    Revert "Merge commit 'e5fdf8335bfa1bff6f4730d1c85b64f974a29a14' into home-page"
    This reverts commit 4733fe032d6fbe070a9bd14824b61042c8843918, reversing
    changes made to 868041b050a26e187618d852c6805288eb1b7bc3.

--- JS/login.js
+++ JS/login.js
@@ -1,7 +1,6 @@
 let btnLogin=document.getElementById("login");
 let formValidation=document.querySelector("form");
 let email=document.getElementById("email");
-<<<<<<< HEAD
 let passValid=document.getElementById("pass-invalid");
 let emailValid=document.getElementById("email-invalid");
 let password=document.getElementById("password");
@@ -14,19 +13,11 @@ let signupBtn=document.getElementById("signup");
 signupBtn.addEventListener("click",()=>{
     window.location.href="registration.html"
 })
-=======
-let password=document.getElementById("password");
-let invalidMsg=document.getElementById("invalid-inputs");
-
-console.log(email)
-console.log(password)
->>>>>>> e5fdf8335bfa1bff6f4730d1c85b64f974a29a14
 
 formValidation.addEventListener("submit",validation);
 
 function validation(e)
 {
-<<<<<<< HEAD
     e.preventDefault()
     if (!formValidation.checkValidity()) 
     {
@@ -43,27 +34,15 @@ function validation(e)
         else if(password.value)
             passValid.classList.add("d-none");
 
-=======
-    if (!formValidation.checkValidity()) 
-    {
-        e.preventDefault()
->>>>>>> e5fdf8335bfa1bff6f4730d1c85b64f974a29a14
         e.stopPropagation()
     }
     else
     {
-<<<<<<< HEAD
         passValid.classList.add("d-none");
         emailValid.classList.add("d-none");
         UserAuthantication()
     }
     // formValidation.classList.add("was-validated");
-=======
-        e.preventDefault()
-        UserAuthantication()
-    }
-    formValidation.classList.add("was-validated");
->>>>>>> e5fdf8335bfa1bff6f4730d1c85b64f974a29a14
 
    
 
@@ -72,22 +51,14 @@ function validation(e)
 function UserAuthantication()
 {
 
-<<<<<<< HEAD
     let tempUsers=JSON.parse(localStorage.getItem("Users")||"[]");
-=======
-    let tempUsers=JSON.parse(localStorage.getItem("Users")||[]);
->>>>>>> e5fdf8335bfa1bff6f4730d1c85b64f974a29a14
     let found=false;
    for(let user of  tempUsers)
     {
         if(user.Email==email.value && user.Password==password.value)
         {
-<<<<<<< HEAD
             // setcookies("Email",user.Email)
             sessionStorage.setItem("CurrentUser",JSON.stringify(user))
-=======
-            setcookies("Email",user.Email)
->>>>>>> e5fdf8335bfa1bff6f4730d1c85b64f974a29a14
             found=true;
             window.location.href="Home.html";
         }
